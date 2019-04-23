ed7fb5fed5f8e07cd713b80b6225d0fe1b6a8c22
deepshreekadus <36604407+deepshreekadus@users.noreply.github.com>
2019-04-23 20:28:20 +0530

    Delete userWishlist.sql

--- userWishlist.sql
+++
@@ -1,2 +0,0 @@
-create table UserWishlist(userID Int not null,companyName varchar(50),arbitrage dec(10,5),quantity Int,currentMarketPrice dec(10,5),transactionDate date,nse dec(9,4),
-bse dec(9,4),foreign key(userID) references userLogin(userID));
