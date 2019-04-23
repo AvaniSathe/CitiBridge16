e884a60c6ffb1f109e071577ac8fb193a9fe4840
deepshreekadus <36604407+deepshreekadus@users.noreply.github.com>
2019-04-23 20:29:13 +0530

    Add files via upload

---
+++ UserWishlist.sql
@@ -0,0 +1,2 @@
+create table userWishlist(userID Int not null,companyName varchar(50),arbitrage dec(10,5),quantity Int,currentMarketPrice dec(10,5),transactionDate date,nse dec(9,4),
+bse dec(9,4),foreign key(userID) references userLogin(userID));
