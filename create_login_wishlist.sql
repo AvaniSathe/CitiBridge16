ed58e2da1eb148aaea29a0e31c81cee0f8ceab4d
deepshreekadus <36604407+deepshreekadus@users.noreply.github.com>
2019-04-23 20:30:28 +0530

    Update create_login_wishlist.sql

--- create_login_wishlist.sql
+++ create_login_wishlist.sql
@@ -23,5 +23,5 @@ insert into UserLogin values(5,"pooja","aixZR7esI2zuDjjamRYQag==");
 
 
 #create UserWishlist table
-create table userWishlist(userID Int not null,companyName varchar(50),arbitrage dec(10,5),quantity Int,currentMarketPrice dec(10,5),transactionDate date,nse dec(9,4),
+create table UserWishlist(userID Int not null,companyName varchar(50),arbitrage dec(10,5),quantity Int,currentMarketPrice dec(10,5),transactionDate date,nse dec(9,4),
 bse dec(9,4),foreign key(userID) references userLogin(userID));
